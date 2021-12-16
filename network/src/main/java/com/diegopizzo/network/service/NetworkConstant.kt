d24c980b9121f6f442464b14ff5208fa7fdcf08c
Diego Pizzo <>
2021-12-16 20:05:27 +0000

    add retrofit interface and unit tests

---
+++ network/src/main/java/com/diegopizzo/network/service/NetworkConstant.kt
@@ -0,0 +1,11 @@
+package com.diegopizzo.network.service
+
+object NetworkConstant {
+    internal const val COMPANY_INFO_ENDPOINT = "company"
+
+    internal const val LAUNCHES_ENDPOINT = "launches"
+
+    internal const val ROCKET_ID = "id"
+    internal const val ROCKET_PATH_ID = "/{$ROCKET_ID}"
+    internal const val ROCKET_ENDPOINT = "rockets"
+}
