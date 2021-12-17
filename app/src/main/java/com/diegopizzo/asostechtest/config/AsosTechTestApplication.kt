dd49e4391aa64a9babdf7b0d6ba00571b3be5e17
Diego Pizzo <>
2021-12-17 17:50:02 +0000

    add application class

---
+++ app/src/main/java/com/diegopizzo/asostechtest/config/AsosTechTestApplication.kt
@@ -0,0 +1,30 @@
+package com.diegopizzo.asostechtest.config
+
+import android.app.Application
+import com.diegopizzo.asostechtest.BuildConfig
+import com.diegopizzo.asostechtest.ui.config.viewModelModule
+import com.diegopizzo.network.cache.cacheInteractorModule
+import com.diegopizzo.network.creator.creatorModule
+import com.diegopizzo.network.interactor.interactorModule
+import com.diegopizzo.network.service.retrofitModule
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class AsosTechTestApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+        startKoin {
+            androidContext(this@AsosTechTestApplication)
+            modules(
+                retrofitModule(BuildConfig.BASE_URL),
+                cacheInteractorModule,
+                creatorModule,
+                interactorModule,
+                viewModelModule
+            )
+        }
+    }
+}
