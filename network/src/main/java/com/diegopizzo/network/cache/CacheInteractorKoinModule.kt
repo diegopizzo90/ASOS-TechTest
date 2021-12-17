23cd205e12be944b65bf95c66af461483d4cc061
Diego Pizzo <>
2021-12-17 17:44:37 +0000

    add logging interception and fix koin module

--- network/src/main/java/com/diegopizzo/network/cache/CacheInteractorKoinModule.kt
+++ network/src/main/java/com/diegopizzo/network/cache/CacheInteractorKoinModule.kt
@@ -3,5 +3,7 @@ package com.diegopizzo.network.cache
 import org.koin.dsl.module
 
 val cacheInteractorModule = module {
-    single<ISpaceXCacheInteractor> { SpaceXCacheInteractor(get(), get()) }
+    single<ISpaceXCacheInteractor> { SpaceXCacheInteractor(get(), TTL_MINUTE) }
 }
+
+private const val TTL_MINUTE = 5L
--- network/src/main/java/com/diegopizzo/network/service/RetrofitKoinModule.kt
+++ network/src/main/java/com/diegopizzo/network/service/RetrofitKoinModule.kt
@@ -1,5 +1,7 @@
 package com.diegopizzo.network.service
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -7,10 +9,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 fun retrofitModule(baseUrl: String) = module {
     single<ApiService> {
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val httpClient = OkHttpClient.Builder().addNetworkInterceptor(interceptor).build()
+
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(httpClient)
             .build()
             .create(ApiService::class.java)
     }
