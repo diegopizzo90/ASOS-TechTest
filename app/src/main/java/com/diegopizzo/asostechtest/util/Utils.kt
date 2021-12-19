7840c0d891c74c1e6c831103a294ab265cf3ecf0
Diego Pizzo <>
2021-12-19 00:38:45 +0000

    add utils function and update tests

--- app/src/main/java/com/diegopizzo/asostechtest/util/Utils.kt
+++ app/src/main/java/com/diegopizzo/asostechtest/util/Utils.kt
@@ -1,11 +1,12 @@
 package com.diegopizzo.asostechtest.util
 
 import org.threeten.bp.ZoneId
+import org.threeten.bp.ZoneOffset
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.temporal.ChronoUnit
 
-object Util {
+object Utils {
 
     fun convertUtcDateTimeToLocalDate(
         utcDate: String,
@@ -31,7 +32,6 @@ object Util {
             )
     }
 
-
     fun getRemainingDays(
         startDateUtc: ZonedDateTime = ZonedDateTime.now(ZoneId.systemDefault()),
         endDateUtc: String
@@ -39,5 +39,30 @@ object Util {
         val endDate = ZonedDateTime.parse(endDateUtc, DateTimeFormatter.ISO_DATE_TIME)
             .withZoneSameInstant(ZoneId.systemDefault())
         return ChronoUnit.DAYS.between(endDate, startDateUtc)
+    }
+
+    fun fromDateStringToZonedDateTimeUtc(dateUtc: String): ZonedDateTime {
+        return ZonedDateTime.parse(dateUtc, DateTimeFormatter.ISO_DATE_TIME)
+            .withZoneSameInstant(ZoneOffset.UTC)
+    }
+
+    fun getYearFromStringDate(dateUtc: String): Int {
+        return fromDateStringToZonedDateTimeUtc(dateUtc).year
+    }
+
+    fun getActualYear(): Int {
+        return ZonedDateTime.now(ZoneId.systemDefault()).year
+    }
+
+    fun isDateUtcGreaterEqualsThan(fromYear: Int, dateUtcString: String): Boolean {
+        val fromDate = ZonedDateTime.of(fromYear, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC)
+        val dateUtc = fromDateStringToZonedDateTimeUtc(dateUtcString)
+        return dateUtc.isAfter(fromDate).or(dateUtc.isEqual(fromDate))
+    }
+
+    fun isDateUtcLowerEqualsThan(toYear: Int, dateUtcString: String): Boolean {
+        val fromDate = ZonedDateTime.of(toYear, 12, 31, 23, 59, 59, 0, ZoneOffset.UTC)
+        val dateUtc = fromDateStringToZonedDateTimeUtc(dateUtcString)
+        return dateUtc.isBefore(fromDate).or(dateUtc.isEqual(fromDate))
     }
 }
